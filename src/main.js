07f478e9ad77037b53a1e441d038023d501da660
Andy164 <apenarandao@ufpso.edu.co>
2021-03-12 11:15:59 -0500

    Crear filtros

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { filterName } from './data.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+// const arr = filterByLetterName(data.results, 'A');
+
+const arr = filterName(data.results);
+
+let list = Array.from(arr);
+
+document.getElementById('names').innerHTML = list;
+
+console.log(arr);
