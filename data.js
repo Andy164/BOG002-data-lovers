e898791c744fc1fda1fa19f31ec47e4ca0a02e97
nicol <nicolmadero12@gmaicom>
2021-03-15 10:06:24 -0500

    Updates

---
+++ data.js
@@ -0,0 +1,15 @@
+// estas funciones son de ejemplo
+
+// export const filterByLetterName = (data, letter) => {
+//   const result = data.filter(character => character.name[0] == letter);
+//   return result;
+// };
+
+export const filterName = (data) => {
+  const result = data.map(character => character.name);
+  return result;
+}
+
+export const anotherExample = () => {
+  return 'OMG';
+};
---
+++ main.js
@@ -0,0 +1,17 @@
+import { filterName } from './data.js';
+import data from './data/rickandmorty/rickandmorty.js';
+
+// const arr = filterByLetterName(data.results, 'A');
+
+const arr = filterName(data.results);
+
+let list = Array.from(arr);
+
+
+document.getElementById('names').innerHTML = list;
+
+    console.log(list);
+
+
+
+
